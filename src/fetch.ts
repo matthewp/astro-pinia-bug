bbdca5d076b7e7a27ed479571b03fb63a1b0c846
devcustrom <devcustrom@gmail.com>
2026-09-01 13:52:12 +0500

    Remove fixed bug

--- src/fetch.ts
+++ src/fetch.ts
@@ -3,11 +3,6 @@ import { FetchState, astro } from 'astro/fetch';
 export default {
 	async fetch(request: Request): Promise<Response> {
 		const state = new FetchState(request);
-
-		state.locals.testlocals = 'Hello Bug';
-		if(request.url.includes('crutch')) {
-			state.renderOptions.locals = state.locals;
-		}
 		return astro(state);
 	},
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,5 @@ import { runWithPinia } from './store/serverStore'
 
 export const onRequest = defineMiddleware((context, next) => {
 	const pinia = createPinia()
-	console.log(context.locals.testlocals)
     return runWithPinia(pinia, () => next())
 })
